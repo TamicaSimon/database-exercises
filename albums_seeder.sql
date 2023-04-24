918207adbc15bc5badd134815d30593f005d8356
Tamica Simon <tamica.simon@gmail.com>
2023-04-24 19:57:02 +0000

    Basic Statements Exercise 1

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '51.2', 'Pop'),
+    ('Eagles', 'Their Greatest Hits', '1976', '41.2', 'Country rock'),
+    ('Eagles', 'Hotel California', '1976', '31.8', 'Soft rock'),
+    ('Shania Twain', 'Come On Over', '1997', '30.4', 'Country'),
+    ('AC/DC', 'Back in Black', '1980', '30.1', 'Hard rock'),
+    ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30.4', 'Hard rock');
+
+
