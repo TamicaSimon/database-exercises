c97b130a3d027ed2323c0c5b0d0e586dfbe1b564
Tamica Simon <tamica.simon@gmail.com>
2023-04-24 20:41:19 +0000

    Basic Statements Exercise 1 update table

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -6,6 +6,27 @@ VALUES ('Michael Jackson', 'Thriller', '1982', '51.2', 'Pop'),
     ('Eagles', 'Hotel California', '1976', '31.8', 'Soft rock'),
     ('Shania Twain', 'Come On Over', '1997', '30.4', 'Country'),
     ('AC/DC', 'Back in Black', '1980', '30.1', 'Hard rock'),
-    ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30.4', 'Hard rock');
+    ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30.4', 'Hard rock'),
+    ('Fleetwood Mac', 'Rumours', '1977', '29.3', 'Soft rock'),
+    ('Whitney Houston', 'The Bodyguard', '1992', '28.7', 'R&B'),
+    ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.8', 'Progressive rock'),
+    ('Bee Gees', 'Saturday Night Fever', '1977', '22.1', 'Disco'),
+    ('Meat Loaf', 'Bat Out of Hell', '1977', '22', 'Hard rock'),
+    ('Adele', '21', '2011', '27.1', 'Pop'),
+    ('Alanis Morissette', 'Jagged Little Pill', '1995', '25.4', 'Alternative rock'),
+    ('Various', 'Dirty Dancing', '1987', '24.1', 'Pop'),
+    ('The Beatles', '1', '2000', '23.2', 'Rock'),
+    ('Pink Floyd', 'The Wall', '1979', '18.9', 'Progressive rock'),
+    ('ABBA', 'Gold: Greatest Hits', '1992', '21.6', 'Pop'),
+    ('The Beatles', 'Abbey Road', '1969', '16.9', 'Rock'),
+    ('The Beatles', 'Sgt Pepper Lonely Hearts Club Band', '1967', '18.3', 'Rock'),
+    ('Celine Dion', 'Falling Into You', '1996', '21.1', 'Pop'),
+    ('Celine Dion', 'Lets Talk About Love', '1997', '20.5', 'Pop'),
+    ('Dire Straits', 'Brothers in Arms', '1985', '21.1', 'Roots rock'),
+    ('Guns N Roses', 'Appetite dor Destruction', '1987', '22.8', 'Hard rock'),
+    ('Michael Jackson', 'Bad', '1987', '21.2', 'Pop'),
+    ('Michael Jackson', 'Dangerous', '1991', '17.0', 'New jack swing'),
+    ('Bob Marley', 'The Best of Bob Marley', '1984', '22.9', 'Reggae');
+
 
 
